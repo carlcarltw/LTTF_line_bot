d1dedc28f82dc4a1dde9ab03bdfe0015c29ed21c
Carl Chen <carlcarltw@gmail.com>
2018-05-30 22:02:46 +0800

    test selenium

--- app.py
+++ app.py
@@ -12,12 +12,12 @@ from linebot.models import (
 
 import requests
 from bs4 import BeautifulSoup
-import re
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
+#import re
+#from selenium import webdriver
+#from selenium.webdriver.common.by import By
+#from selenium.webdriver.support.ui import WebDriverWait
+#from selenium.webdriver.support import expected_conditions as EC
+#from selenium.common.exceptions import TimeoutException
 
 app = Flask(__name__)
 
