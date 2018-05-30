cbab391a91465289ac6ec02de0e3ee1e8b1fa4af
Carl Chen <carlcarltw@gmail.com>
2018-05-30 23:06:52 +0800

    test selenium4

--- app.py
+++ app.py
@@ -122,7 +122,7 @@ def crawl_courts_data(courts_name):
 
     # download the chrome driver from https://sites.google.com/a/chromium.org/chromedriver/downloads and put it in the
     # current directory
-    chrome_driver = os.getcwd() +"/chromedriver.exe"
+    chrome_driver = os.getcwd() +"/chromedriver"
 
     # go to Google and click the I'm Feeling Lucky button
     broswer = webdriver.Chrome(chrome_options=chrome_options, executable_path=chrome_driver)
